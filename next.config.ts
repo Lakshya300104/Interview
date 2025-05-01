bfd2bbad55609c877208d1b3d181d50b112408cd
Lakshya Arora <lakshya300104@gmail.com>
2025-05-01 06:40:37 +0530

    again

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    esbuild: {
+    /* config options here */
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: "https",
+    //       hostname: "ik.imagekit.io",
+    //       port: "",
+    //     },
+    //   ],
+    // },
+    eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
 };
 
 export default nextConfig;
